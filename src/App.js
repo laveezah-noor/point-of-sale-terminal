1a71244a23f587315ae1eab1da6c194d8c0ebf66
laveezah-noor <laveezahnoor@gmail.com>
2023-07-14 12:44:10 +0500

    routes file made

--- src/App.js
+++ src/App.js
@@ -3,48 +3,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import DashboardPage from './routes/dashboard-page';
-import InventoryPage from './routes/inventory-page';
-import NewEntryPage from './routes/newEntry-page';
-import ErrorPage from './routes/error-page';
 import Sidebar from './components/sidebar';
 import Navbar from './components/navbar';
-import { ProductsPage } from './routes/products-page';
-import  {newProductPage}  from './routes/newProduct-page';
-import { OrderPage } from './routes/order-page';
+import routes from './routes'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DashboardPage/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/inventory",
-    element: <InventoryPage/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/newEntry",
-    element: <NewEntryPage/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/product",
-    element: <ProductsPage/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/newProduct",
-    element: <newProductPage/>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: "/newOrder",
-    element: <OrderPage/>,
-    errorElement: <ErrorPage/>
-  },
-]);
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
